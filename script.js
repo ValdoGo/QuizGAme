325f67fb25be5b209c5c6413bce54c205ef52f29
ValdoGo <citacoesempt@gmail.com>
2026-04-17 14:40:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const valorPalpite = document.getElementById("valor-input");
+const reiniciarQuiz = document.getElementById("restart-btn");
+const enviarPalpite = document.getElementById("try-btn");
+const mensagem = document.getElementById("msg");
+const containerResultados = document.querySelector(".resultados"); // Coloquei aqui fora para facilitar
+
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+
+// Event Listeners
+enviarPalpite.addEventListener("click", sendAnswer);
+reiniciarQuiz.addEventListener("click", restartQuiz);
+
+function sendAnswer() {
+    const palpite = Number(valorPalpite.value);  
+
+    if (valorPalpite.value === "") {
+        mensagem.textContent = "O campo não pode estar vazio";
+        return;
+    }
+
+    if (palpite === numeroSecreto) {
+        mensagem.textContent = "Você acertou, PARABÉNS! Reiniciando em 5 segundos...";
+        enviarPalpite.disabled = true;
+
+        setTimeout(() => {
+            restartQuiz();
+            enviarPalpite.disabled = false;
+          
+        }, 5000);
+
+    } else {
+        mensagem.textContent = "Você errou! Veja a dica abaixo:";
+        
+        let iconeSeta = (palpite > numeroSecreto) ? "arrow_downward" : "arrow_upward";
+
+        containerResultados.innerHTML += `
+            <div class="r-esult">
+                <i class="number">${palpite}</i>
+                <span class="material-symbols-outlined">${iconeSeta}</span>
+            </div>
+        `;
+
+        valorPalpite.value = "";
+        valorPalpite.focus();
+    }
+}
+
+function restartQuiz() {
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    valorPalpite.value = "";
+    mensagem.textContent = "Jogo reiniciado! Tente adivinhar.";
+    
+    // LIMPA O HISTÓRICO
+    containerResultados.innerHTML = "Historicos:";
+    
+    console.log("Novo número secreto:", numeroSecreto);
+}
